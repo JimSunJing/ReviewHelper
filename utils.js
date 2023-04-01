e98af17697e0a6c32e4b0b4485893d2e0293466f
JimS <jimsun6428@gmail.com>
2023-04-01 17:25:09 +0800

    fix title and rich_text bug

--- utils.js
+++ utils.js
@@ -3,12 +3,12 @@ const { floor, random } = Math;
 // 输入数组并随机返回
 function randomSelect(arr) {
   // 非空判断
-  if (typeof arr === 'undefined') {
+  if (typeof arr === "undefined") {
     console.log(arr);
-    return "no data"
+    return "no data";
   }
   let len = arr.length;
-  let choice = floor(random()*len);
+  let choice = floor(random() * len);
   // console.log(`len=${len} choice=${choice}`);
   return arr[choice];
 }
@@ -17,56 +17,64 @@ function randomSelect(arr) {
 // colName: the wanted column name
 function getNotionProperty(obj, colName) {
   // 非空判断
-  if (typeof obj === 'undefined') {
+  if (typeof obj === "undefined") {
     console.log(obj);
-    return "no data"
+    return "no data";
   }
   let prop;
-  if (typeof obj.properties === 'undefined'){
-    return 'No properties';
+  if (typeof obj.properties === "undefined") {
+    return "No properties";
   } else {
     prop = obj.properties[String(colName)];
     // console.log(`get prop! ${prop}`);
   }
   switch (prop.type) {
-    case 'title':
-      return prop.title[0].plain_text;
+    case "title":
+      return prop.title
+        .map((item) => item.plain_text.replace(/\n/g, ""))
+        .join(" ");
       break;
-    case 'rich_text':
-      return prop.rich_text[0] === undefined 
-        ? `'${String(colName)}' is empty` : prop.rich_text[0].plain_text;
-      break
-    case 'checkbox':
+    case "rich_text":
+      const texts =
+        prop.rich_text[0] &&
+        prop.rich_text
+          .map((item) => item.plain_text.replace(/\n/g, ""))
+          .join(" ");
+      return texts ? `'${String(colName)}' is empty` : texts;
+      break;
+    case "checkbox":
       return prop.checkbox === true;
-      break
-    case 'select':
-      return prop.select === undefined 
+      break;
+    case "select":
+      return prop.select === undefined
         ? `'${String(colName)}'\(select\) is empty`
         : prop.select.name;
-      break
-    case 'created_time':
+      break;
+    case "created_time":
       return prop.created_time;
-      break
-    case 'url':
+      break;
+    case "url":
       return prop.url === undefined || prop.url === null
-        ? `'${String(colName)}'\(url\) is empty` : prop.url
-      break
-    case 'date':
+        ? `'${String(colName)}'\(url\) is empty`
+        : prop.url;
+      break;
+    case "date":
       return prop.date === null
-        ? `'${String(colName)}'\(date\) is empty` : prop.date
-      break
-    case 'number':
-      return prop.number
-      break
-    case 'phone':
-      return prop.phone_number
-      break
-    case 'email':
-      return prop.email
-      break
-    case 'multi_select':
+        ? `'${String(colName)}'\(date\) is empty`
+        : prop.date;
+      break;
+    case "number":
+      return prop.number;
+      break;
+    case "phone":
+      return prop.phone_number;
+      break;
+    case "email":
+      return prop.email;
+      break;
+    case "multi_select":
       let names = [];
-      if (prop.multi_select === undefined || prop.multi_select.length === 0){
+      if (prop.multi_select === undefined || prop.multi_select.length === 0) {
         return `'${String(colName)}'\(date\) is empty`;
       } else {
         for (let i = 0; i < prop.multi_select.length; i++) {
@@ -75,8 +83,8 @@ function getNotionProperty(obj, colName) {
         }
       }
       return names;
-      break
-      
+      break;
+
     default:
       break;
   }
@@ -84,5 +92,5 @@ function getNotionProperty(obj, colName) {
 
 module.exports = {
   randomSelect,
-  getNotionProperty
-}
+  getNotionProperty,
+};
